4accdfc6c6173a829564673e4b2b93c4a315209f
szjunior <kR40n8vz@git>
2024-05-08 15:36:55 -0300

    app space

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotografia
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "publicada")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_editable = ("publicada",)
+    list_per_page = 20
+
+
+
+admin.site.register(Fotografia, ListandoFotografias)
+
+
