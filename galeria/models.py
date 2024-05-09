d9274a7791c38aadb7e5b98281140ef6905bab74
szjunior <kR40n8vz@git>
2024-05-09 15:52:28 -0300

    app space

--- galeria/models.py
+++ galeria/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+
 from datetime import datetime
+
+from django.contrib.auth.models import User
 
 class Fotografia(models.Model):
 
     OPCOES_CATEGORIA = [
-        ("NEBULOSA", "Nebulosa"),
-        ("ESTRELA", "Estrela"),
-        ("GALÁXIA", "Galáxia"),
-        ("PLANETA", "Planeta"),
-        
+        ("NEBULOSA","Nebulosa"),
+        ("ESTRELA","Estrela"),
+        ("GALÁXIA","Galáxia"),
+        ("PLANETA","Planeta"),
     ]
-
 
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
@@ -19,10 +20,13 @@ class Fotografia(models.Model):
     foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+    usuario = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name='user'
+    )
 
-
-    def __str__(self) -> str:
+    def __str__(self):
         return self.nome
-    
-    
-
